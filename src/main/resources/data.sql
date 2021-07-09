6ca1c6504c68ee80ab337b451aca754ba5461d8b
suuuuzy <jangsooji91@gmail.com>
2021-07-09 23:19:54 +0900

    JPA를 이용한 사용자 조회, 추가, 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1, sysdate(), 'User1', 'test1111', '911012-2222222');
-insert into user values(2, sysdate(), 'User2', 'test2222', '811012-1111111');
-insert into user values(3, sysdate(), 'User3', 'test3333', '711012-2222222');
+insert into user values(90001, sysdate(), 'User1', 'test1111', '911012-2222222');
+insert into user values(90002, sysdate(), 'User2', 'test2222', '811012-1111111');
+insert into user values(90003, sysdate(), 'User3', 'test3333', '711012-2222222');
